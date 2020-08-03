6a76cc03817df6855f36320f88d6dc8522b8af26
Debora Ermel <debbyermel@gmail.com>
2020-08-03 09:58:47 -0700

    Added bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :articles
-  validates : username, presence: true, uniqueness: { case_sensitive: false },
+  validates :username, presence: true,
+    uniqueness: { case_sensitive: false },
     length: {minimum: 3, maximum: 25 }
-  validates : email, presence: true, uniqueness: { case_sensitive: false },
+  validates :email, presence: true,
+    uniqueness: { case_sensitive: false },
     length: { maximum: 105 }
+  has_secure_password
 end
