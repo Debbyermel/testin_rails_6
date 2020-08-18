22ff6bc8f491367e1e8a932a883bf966ac113e7c
Debora Ermel <debbyermel@gmail.com>
2020-08-18 13:47:25 -0700

    Add index and show category for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    byebug
     # render plain: @article.inspect
     @article = Article.new(article_params)
     @article.user = current_user
