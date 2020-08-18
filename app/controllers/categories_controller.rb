1d1c0f940311a418cbbcfa05cbdeec0f672d7f1c
Debora Ermel <debbyermel@gmail.com>
2020-08-17 17:11:38 -0700

    Admin privileges for creating a new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :required_admin, except: [:index, :show]
 
   def new
     @category = Category.new
@@ -26,6 +27,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.required(:category).permit(:name)
+  end
+
+  def required_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = 'Only Admins are allowed to perform this action.'
+      redirect_to categories_path
+    end
   end
 
 end
